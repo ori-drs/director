a91df7478c27c3479e099cbe4268e72527158435
Maurice Fallon <mauricefallon@gmail.com>
2017-08-23 15:10:40 +0100

    LIDAR and point cloud segmentation fixes (partial)
    In vtkLidarSource:
    not quite sure of what this is doing, but it compiles

--- src/vtk/DRCFilters/vtkLidarSource.cxx
+++ src/vtk/DRCFilters/vtkLidarSource.cxx
@@ -807,14 +807,13 @@ int vtkLidarSource::RequestData(
   vtkDataSet *output = vtkDataSet::SafeDownCast(info->Get(vtkDataObject::DATA_OBJECT()));
 
   int timestep = 0;
-// fixme mfallon
-/*
-  if (info->Has(vtkStreamingDemandDrivenPipeline::UPDATE_TIME_STEPS()))
+  // This was changed when updating to 16.06 - but I couldn't test it
+  // - mfallon
+  if (info->Has(vtkStreamingDemandDrivenPipeline::UPDATE_TIME_STEP()))
     {
-    double timeRequest = info->Get(vtkStreamingDemandDrivenPipeline::UPDATE_TIME_STEPS())[0];
+    double timeRequest = info->Get(vtkStreamingDemandDrivenPipeline::UPDATE_TIME_STEP());
     timestep = static_cast<int>(floor(timeRequest+0.5));
     }
-*/
   this->Internal->Listener->SetDistanceRange(this->DistanceRange);
   this->Internal->Listener->SetHeightRange(this->HeightRange);
   vtkSmartPointer<vtkPolyData> polyData = this->Internal->Listener->GetDataForRevolution(timestep);
