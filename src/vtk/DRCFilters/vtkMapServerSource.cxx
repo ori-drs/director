c55f26e5899982a95da032971db4c58b4c753945
mauricefallon@gmail.com <mauricefallon@gmail.com>
2017-12-21 11:37:48 +0000

    maps server works

--- src/vtk/DRCFilters/vtkMapServerSource.cxx
+++ src/vtk/DRCFilters/vtkMapServerSource.cxx
@@ -43,7 +43,7 @@
 #include <lcmtypes/maps/scans_t.hpp>
 #include <lcmtypes/maps/data_request_t.hpp>
 
-#include <maps/LcmTranslator.hpp>
+#include <maps_lcm/LcmTranslator.hpp>
 #include <maps/DepthImageView.hpp>
 #include <maps/DepthImage.hpp>
 #include <maps/PointCloudView.hpp>
@@ -560,14 +560,13 @@ protected:
     int width = depthImage->getWidth();
     int height = depthImage->getHeight();
 
+    // this wasnt fixed during the upgrade/ mfallon:
     vtkSmartPointer<vtkImageData> image = vtkSmartPointer<vtkImageData>::New();
-    image->SetWholeExtent(0, width-1, 0, height-1, 0, 0);
+    image->SetExtent(0, width-1, 0, height-1, 0, 0);   
     image->SetSpacing(1.0, 1.0, 1.0);
     image->SetOrigin(0.0, 0.0, 0.0);
-    image->SetExtent(image->GetWholeExtent());
-    image->SetNumberOfScalarComponents(1);
-    image->SetScalarType(VTK_FLOAT);
-    image->AllocateScalars();
+    image->SetExtent(image->GetExtent());
+    image->AllocateScalars(VTK_FLOAT, 1);
 
     std::vector<float> imageData = depthImage->getData(maps::DepthImage::TypeDepth);
 
@@ -919,9 +918,10 @@ int vtkMapServerSource::RequestData(
   vtkDataSet *output = vtkDataSet::SafeDownCast(info->Get(vtkDataObject::DATA_OBJECT()));
 
   int timestep = 0;
-  if (info->Has(vtkStreamingDemandDrivenPipeline::UPDATE_TIME_STEPS()))
+  // This was changed when updating to 16.06 - but I couldn't test it
+  if (info->Has(vtkStreamingDemandDrivenPipeline::UPDATE_TIME_STEP()))
     {
-    double timeRequest = info->Get(vtkStreamingDemandDrivenPipeline::UPDATE_TIME_STEPS())[0];
+    double timeRequest = info->Get(vtkStreamingDemandDrivenPipeline::UPDATE_TIME_STEP());
     timestep = static_cast<int>(floor(timeRequest+0.5));
     }
 
