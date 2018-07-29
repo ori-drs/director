bc4235cb092813d8510af6c6a64aa0bc7bc278fd
Maurice Fallon <mauricefallon@gmail.com>
2018-07-29 20:26:40 +0100

    lengthen range

--- src/python/director/segmentation.py
+++ src/python/director/segmentation.py
@@ -171,8 +171,8 @@ class DisparityPointCloudItem(vis.PolyDataItem):
 
         self.addProperty('Decimation', 2, attributes=om.PropertyAttributes(enumNames=['1', '2', '4', '8', '16']))
         self.addProperty('Remove Size', 1000, attributes=om.PropertyAttributes(decimals=0, minimum=0, maximum=100000.0, singleStep=1000))
-        self.addProperty('Target FPS', 5.0, attributes=om.PropertyAttributes(decimals=1, minimum=0.1, maximum=30.0, singleStep=0.1))
-        self.addProperty('Max Range', 2.0,  attributes=om.PropertyAttributes(decimals=2, minimum=0., maximum=30.0, singleStep=0.25))
+        self.addProperty('Target FPS', 10.0, attributes=om.PropertyAttributes(decimals=1, minimum=0.1, maximum=30.0, singleStep=0.1))
+        self.addProperty('Max Range', 5.0,  attributes=om.PropertyAttributes(decimals=2, minimum=0., maximum=30.0, singleStep=0.25))
 
         self.timer = TimerCallback()
         self.timer.callback = self.update
