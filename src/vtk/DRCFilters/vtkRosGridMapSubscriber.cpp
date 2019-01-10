502a78995f6277b8cdaa1e21a7bede5d6a4a7bff
Benoit Casseau <benoit@oxfordrobotics.institute>
2019-01-10 09:54:07 +0000

    Speed up height map

--- src/vtk/DRCFilters/vtkRosGridMapSubscriber.cpp
+++ src/vtk/DRCFilters/vtkRosGridMapSubscriber.cpp
@@ -59,18 +59,20 @@ void vtkRosGridMapSubscriber::GridMapCallback(const grid_map_msgs::GridMap& mess
   dataset_ = ConvertMesh(inputMap);
 }
 
+#include "boost/date_time/posix_time/posix_time.hpp" //include all types plus i/o
+
 vtkSmartPointer<vtkPolyData> vtkRosGridMapSubscriber::ConvertMesh(grid_map::GridMap& inputMap)
 {
+  boost::posix_time::ptime mst1 = boost::posix_time::microsec_clock::local_time();
+
   inputMap.convertToDefaultStartIndex();
   const size_t rows = inputMap.getSize()(0);
   const size_t cols = inputMap.getSize()(1);
-  long int count_point = 0;
 
   vtkSmartPointer<vtkPolyData> polyData = vtkSmartPointer<vtkPolyData>::New();
   vtkSmartPointer<vtkCellArray> cellArray = vtkSmartPointer<vtkCellArray>::New();
   vtkSmartPointer<vtkPoints> points = vtkSmartPointer<vtkPoints>::New();
   points->SetDataTypeToDouble();
-
 
   const std::vector< std::string > &  layers = inputMap.getLayers();
   // initialize colors, one for each layers
@@ -92,11 +94,14 @@ vtkSmartPointer<vtkPolyData> vtkRosGridMapSubscriber::ConvertMesh(grid_map::Grid
     return polyData;
   }
 
+  std::vector<std::vector<grid_map::Position3> > vertices((rows-1)*(cols-1));
+  std::vector<std::vector<grid_map::Index> > indexes((rows-1)*(cols-1));
+  int numPoints = 0;
+  int numCells = 0;
+  // first loop to compute the number of vertices and the number of cells
   for (size_t i = 0; i < rows - 1; ++i) {
     for (size_t j = 0; j < cols - 1; ++j) {
 
-      std::vector<grid_map::Position3> vertices;
-      std::vector<grid_map::Index> indexes;
       for (size_t k = 0; k < 2; k++) {
         for (size_t l = 0; l < 2; l++) {
           grid_map::Position3 position;
@@ -106,41 +111,67 @@ vtkSmartPointer<vtkPolyData> vtkRosGridMapSubscriber::ConvertMesh(grid_map::Grid
           }
 
           inputMap.getPosition3(layer_name, index, position);
-          vertices.push_back(position);
-          indexes.push_back(index);
+          vertices[i*(cols-1) + j].push_back(position);
+          indexes[i*(cols-1) + j].push_back(index);
         }
       }
-      if (vertices.size() > 2) {
-        for (size_t m = 1; m < vertices.size() - 1; m++) {
-          points->InsertNextPoint(vertices[m-1](0), vertices[m-1](1), vertices[m-1](2));
-          points->InsertNextPoint(vertices[m](0), vertices[m](1), vertices[m](2));
-          points->InsertNextPoint(vertices[m+1](0), vertices[m+1](1), vertices[m+1](2));
-
-          vtkSmartPointer<vtkTriangle> triangle =
-              vtkSmartPointer<vtkTriangle>::New();
-          triangle->GetPointIds()->SetId(0, count_point);
-          triangle->GetPointIds()->SetId(1, count_point + 1);
-          triangle->GetPointIds()->SetId(2, count_point + 2);
-          cellArray->InsertNextCell(triangle);
-          count_point += 3;
-
-          //colors
-          for(int i = 0; i< layers.size(); ++i) {
-            unsigned char color[3];
-            getInterpolatedColor(inputMap, layers[i], indexes[m-1],
-                                 minIntensity[i], maxIntensity[i], color);
-            colors[i]->InsertNextTupleValue(color);
-            getInterpolatedColor(inputMap, layers[i], indexes[m],
-                                 minIntensity[i], maxIntensity[i], color);
-            colors[i]->InsertNextTupleValue(color);
-            getInterpolatedColor(inputMap, layers[i], indexes[m+1],
-                                 minIntensity[i], maxIntensity[i], color);
-            colors[i]->InsertNextTupleValue(color);
-          }
-
-        }
+      if (vertices[i*(cols-1) + j].size() == 3) {
+        numPoints += 3;
+        numCells += 1;
+      } else if (vertices[i*(cols-1) + j].size() == 4) {
+        numPoints += 6;
+        numCells += 2;
       }
 
+    }
+  }
+  points->SetNumberOfPoints(numPoints);
+
+  // populating the cells, the following code is equivalent to
+  /*vtkSmartPointer<vtkTriangle> triangle = vtkSmartPointer<vtkTriangle>::New();
+  triangle->GetPointIds()->SetId(0, count_point);
+  triangle->GetPointIds()->SetId(1, count_point + 1);
+  triangle->GetPointIds()->SetId(2, count_point + 2);
+  cellArray->InsertNextCell(triangle);*/
+  vtkSmartPointer<vtkIdTypeArray> cellsPtr = vtkIdTypeArray::New();
+  vtkIdType* vtkCells = (vtkIdType*)malloc(4*sizeof(vtkIdType)*numCells);
+  vtkIdType* ptr = vtkCells;
+  for(int i = 0; i < numCells; ++i) {
+    *ptr = 3;
+    *(ptr + 1) = i*3;
+    *(ptr + 2) = i*3 + 1;
+    *(ptr + 3) = i*3 + 2;
+    ptr += 4;
+  }
+  cellsPtr->SetArray(vtkCells, 4*numCells, 0);
+  cellArray->SetCells(numCells, cellsPtr);
+
+  int count_point = 0;
+  for (size_t j = 0; j < vertices.size(); ++j) {
+    if (vertices[j].size() > 2) {
+      for (size_t m = 1; m < vertices[j].size() - 1; m++) {
+        points->SetPoint(count_point, vertices[j][m-1](0), vertices[j][m-1](1), vertices[j][m-1](2));
+        points->SetPoint(count_point+1, vertices[j][m](0), vertices[j][m](1), vertices[j][m](2));
+        points->SetPoint(count_point+2, vertices[j][m+1](0), vertices[j][m+1](1), vertices[j][m+1](2));
+
+        count_point += 3;
+        //colors
+        // about 50msec for other layers
+        for(int i = 0; i< layers.size(); ++i) {
+          unsigned char color[3];
+
+          getInterpolatedColor(inputMap, layers[i], indexes[j][m-1],
+              minIntensity[i], maxIntensity[i], color);
+          colors[i]->InsertNextTupleValue(color);
+          getInterpolatedColor(inputMap, layers[i], indexes[j][m],
+                               minIntensity[i], maxIntensity[i], color);
+          colors[i]->InsertNextTupleValue(color);
+          getInterpolatedColor(inputMap, layers[i], indexes[j][m+1],
+              minIntensity[i], maxIntensity[i], color);
+          colors[i]->InsertNextTupleValue(color);
+        }
+
+      }
     }
   }
 
@@ -149,6 +180,12 @@ vtkSmartPointer<vtkPolyData> vtkRosGridMapSubscriber::ConvertMesh(grid_map::Grid
     polyData->GetPointData()->AddArray(colors[i]);
   }
   polyData->SetPolys(cellArray);
+
+  boost::posix_time::time_duration msdiff;
+  msdiff = boost::posix_time::microsec_clock::local_time() - mst1;
+  std::cout << msdiff.total_milliseconds() << " elapsed (elevation map)" << std::endl;
+  std::cout << std::endl;
+
   return polyData;
 }
 
