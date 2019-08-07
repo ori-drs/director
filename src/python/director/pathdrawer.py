8ca3df5096c92d39f882b2bff96d445691826c82
Benoit Casseau <benoit@oxfordrobotics.institute>
2019-08-07 10:24:08 +0100

    Add lines between frames

--- src/python/director/pathdrawer.py
+++ src/python/director/pathdrawer.py
@@ -1,6 +1,7 @@
 import director.objectmodel as om
 import director.rosutils as rosutils
 from director import tfdrawer as tf_draw
+import director.applogic as app
 
 from nav_msgs.msg import Path
 
@@ -9,22 +10,46 @@ class AicpPoseSource(om.ContainerItem):
     def __init__(self, name, topicName, fixedFrame):
         om.ContainerItem.__init__(self, name)
 
+        om.addToObjectModel(self)
         self.topicName = topicName
         self.subscriber = rosutils.addSubscriber(self.topicName, Path, self._posesCallback)
         self.tfDrawer = tf_draw.TfDrawer(fixedFrame)
-        self.objects = [] # objects drawn by this class
+        self.lines = []
+        self.frames = []
+        self.lineContainer = om.getOrCreateContainer('lines', parentObj=self)
 
         self.addProperty('Topic name', topicName)
         self.addProperty('Subscribe', True)
         self.addProperty('Visible', True)
+        self.addProperty('Draw Lines', True)
+        self.addProperty('Draw Frames', True)
 
 
     def _posesCallback(self, msg):
 
+        view = app.getCurrentRenderView()
+
+        # clear lines and frames
+        for line in self.lines:
+            line.disconnect()
+        del self.lines[:]
+        del self.frames[:]
+
+
+        prevTfFrame = None
         for i, pose in enumerate(msg.poses):
             transform = rosutils.rosPoseToTransform(pose.pose)
-            self.tfDrawer.drawFrame(transform, "pose " + str(i), msg.header.stamp, msg.header.frame_id,
+            tfFrame = self.tfDrawer.drawFrame(transform, "pose " + str(i), msg.header.stamp, msg.header.frame_id,
                                     parent=self)
+            self.frames.append(tfFrame)
+
+            if prevTfFrame:
+                line = tf_draw.LineItem('line ' + str(i), prevTfFrame, tfFrame)
+                line.addToView(view)
+                om.addToObjectModel(line, self.lineContainer)
+                self.lines.append(line)
+            prevTfFrame = tfFrame
+
 
     def _onPropertyChanged(self, propertySet, propertyName):
         om.ContainerItem._onPropertyChanged(self, propertySet, propertyName)
@@ -38,4 +63,14 @@ class AicpPoseSource(om.ContainerItem):
                 self.subscriber = rosutils.addSubscriber(self.topicName, Path, self._posesCallback)
             else:
                 self.subscriber = None
+        elif propertyName == 'Draw Lines':
+            for line in self.lines:
+                line.setProperty('Visible', self.getProperty(propertyName))
+        elif propertyName == 'Draw Frames':
+            for frame in self.frames:
+                frame.setProperty('Visible', self.getProperty(propertyName))
+        elif propertyName == 'Visible':
+            self.setProperty('Draw Lines', self.getProperty(propertyName))
+            self.setProperty('Draw Frames', self.getProperty(propertyName))
+
 
--- src/python/director/tfdrawer.py
+++ src/python/director/tfdrawer.py
@@ -3,6 +3,37 @@ from director import visualization as vis
 from director.debugVis import DebugData
 from director import objectmodel as om
 
+import vtk
+
+
+class LineItem(vis.PolyDataItem):
+    def __init__(self, name, tfFrame1, tfFrame2):
+        vis.PolyDataItem.__init__(self, name, vtk.vtkPolyData(), view=None)
+        self.tfFrame1 = tfFrame1
+        self.tfFrame2 = tfFrame2
+        self.callbackId1 = tfFrame1.connectTransformModified(self.redraw())
+        self.callbackId2 = tfFrame2.connectTransformModified(self.redraw())
+        self.draw()
+
+    def draw(self):
+        d = DebugData()
+        p1 = self.tfFrame1.transform.GetPosition()
+        p2 = self.tfFrame2.transform.GetPosition()
+        d.addLine(p1, p2, color=[0, 1, 0])
+        polyData = d.getPolyData()
+        self.setPolyData(polyData)
+        self.setProperty('Color By', 'RGB255')
+
+    def disconnect(self):
+        self.tfFrame1.disconnectCallback(self.callbackId1)
+        self.tfFrame2.disconnectCallback(self.callbackId2)
+
+    def redraw(self):
+        #callback called when a TfFrame1 or TfFrame2 is modified
+        self.draw()
+
+
+
 class TfDrawer(object):
 
     def __init__(self, frame):
