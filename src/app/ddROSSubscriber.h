00b94a82cddba98f24c97b438253be69fc14ecfb
Michal Staniaszek <m.staniaszek@gmail.com>
2020-01-06 15:05:39 +0000

    fix ifndef, move ros init to separate class

--- src/app/ddROSSubscriber.h
+++ src/app/ddROSSubscriber.h
@@ -1,5 +1,5 @@
-#ifndef __ddROSStateSubscriber_h
-#define __ddROSStateSubscriber_h
+#ifndef __ddROSSubscriber_h
+#define __ddROSSubscriber_h
 
 #include <QObject>
 #include <ddMacros.h>
@@ -25,26 +25,6 @@ public:
     : QObject(parent)
   {
     mChannel = channel;
-
-    if (!ros::isInitialized()) {
-      // guarantee contiguous, null terminated strings
-      std::vector<std::vector<char>> vstrings;
-      // pointers to rhose strings
-      std::vector<char*> cstrings;
-      vstrings.reserve(argv2.size());
-      cstrings.reserve(argv2.size());
-
-      for(size_t i = 0; i < argv2.size(); ++i) {
-        std::string argv_string = argv2[i].toStdString();
-        vstrings.emplace_back(argv_string.begin(), argv_string.end());
-        vstrings.back().push_back('\0');
-        cstrings.push_back(vstrings.back().data());
-      }
-      int argc = cstrings.size();
-
-      ros::init(argc, cstrings.data(), "director_dd", ros::init_options::NoSigintHandler |
-                ros::init_options::AnonymousName);
-    }
 
     ros::NodeHandle n;
     std::string channel_str = channel.toLocal8Bit().data();
