f2a9a0da39287aa10faffb571df65ea3a2916008
Benoit Casseau <benoit@oxfordrobotics.institute>
2020-01-08 09:22:31 +0000

    fix tests

--- src/python/tests/testValveFitStereo.py
+++ src/python/tests/testValveFitStereo.py
@@ -22,7 +22,7 @@ segmentationroutines.SegmentationContext.initWithRobot(robotStateModel)
 
 # load poly data
 dataDir = app.getTestingDataDirectory()
-polyData = ioUtils.readPolyData(os.path.join(dataDir, 'valve/valve-sparse-stereo.pcd'))
+polyData = ioUtils.readPolyData(os.path.join(dataDir, 'misc/valve/valve-sparse-stereo.pcd'))
 vis.showPolyData(polyData, 'pointcloud snapshot original', colorByName='rgb_colors')
 polyData = segmentationroutines.sparsifyStereoCloud( polyData )
 vis.showPolyData(polyData, 'pointcloud snapshot')
