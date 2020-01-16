a5872f1f5f69d106d7d914e17290a2068b55e365
Michal Staniaszek <m.staniaszek@gmail.com>
2020-01-16 12:25:56 +0000

    fix tf_vis related files to remove dependency, fix accidentally removed file in cmake

--- src/python/director/startup.py
+++ src/python/director/startup.py
@@ -89,7 +89,6 @@ from director import segmentationpanel
 #from director import lcmUtils
 from director.utime import getUtime
 from director.shallowCopy import shallowCopy
-from director import tfvisualization as tf_vis
 
 from director import segmentationroutines
 from director import trackers
@@ -248,15 +247,14 @@ if not useLightColorScheme:
     viewBackgroundLightHandler.action.trigger()
 
 # reset time button and connections
-button = QtGui.QPushButton('')
-button.text = 'Reset time'
+button = QtGui.QPushButton('Reset time')
+button.setObjectName("resettime")
 button.connect('clicked()', pointCloudSource.resetTime)
 button.connect('clicked()', gridMapSource.resetTime)
 button.connect('clicked()', headCameraPointCloudSource.resetTime)
 button.connect('clicked()', groundCameraPointCloudSource.resetTime)
 button.connect('clicked()', robotStateJointController.resetTime)
 button.connect('clicked()', cameraview.cameraView.resetTime)
-button.connect('clicked()', tf_vis.TfFrameSync.resetTime)
 app.getMainWindow().statusBar().addPermanentWidget(button)
 
 
--- src/python/director/viewframes.py
+++ src/python/director/viewframes.py
@@ -1,7 +1,7 @@
 from director import objectmodel as om
 import director.visualization as vis
-import director.tfvisualization as tf_vis
 import director.vtkAll as vtk
+
 
 class ViewFramesSizeHandler(object):
     """
@@ -12,25 +12,16 @@ class ViewFramesSizeHandler(object):
         self.action.checkable = True
         self.action.connect('triggered()', self._onChecked)
 
-
     def _onChecked(self):
 
         if self.action.checked:
-            tf_vis.TfFrameItem.setBigSizeProperties()
             vtk.vtkFrameWidgetRepresentation.SetBigSizeHandles()
         else:
-            tf_vis.TfFrameItem.setDefaultSizeProperties()
             vtk.vtkFrameWidgetRepresentation.SetDefaultSizeHandles()
 
         for obj in om.getObjects():
-            if type(obj) is tf_vis.TfFrameItem:
-                obj.setSizeProperties()
-
             if obj.getProperty('Name') == 'walking goal' and type(obj) is vis.FrameItem:
                 # increase size of the tool to move walking goal
                 rep = obj.widget.GetRepresentation()
                 rep.RedrawHandles()
                 obj._renderAllViews()
-
-
-
